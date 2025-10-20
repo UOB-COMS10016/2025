31f9ce97fc6ee675b615473f0a3483e9b08c2b16
ratherforky <34199683+ratherforky@users.noreply.github.com>
2025-10-20 22:54:29 +0100

    add template to folds lecture

--- functionalPageDSL.hs
+++ functionalPageDSL.hs
@@ -243,11 +243,12 @@ functionalPage =
           spec =
             Lecture
               { slot = Second,
-                slidesFile' = Nothing,
+                slidesFile' = Just (BBLectureCode "FoldsPart1Template.hs"),
                 lectureRecording = Nothing
               },
           materials =
             [ minSheet "https://forms.office.com/e/0TFiDaNH9J"
+            , lectureCode "FoldsPart1Template.hs"
             ]
         },
       Entry
