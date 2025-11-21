026c768ac3fd80564224fbe3abd3447db1a059bd
TiloBurghardt <tb2935@bristol.ac.uk>
2025-11-21 14:25:49 +0000

    Update imperative.js

--- imperative.js
+++ imperative.js
@@ -149,12 +149,12 @@ var activities = [
 ["10", "",                     "",                                 "", "", "0", "0"],
 ["0", "Thu 21/11/24<br/>12:00pm-12:50pm<br/>CHEM BLDG LT1",   "DESIGN", "https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2025_TB-1/content/imperative/COMS10016_22_Design_NA.pdf", "https://mediasite.bris.ac.uk/Mediasite/Play/f205eea1d0014aa286f0bf98d613ee5f1d",  "112", "1"],
 ["10", "",                     "",                                 "", "", "0", "0"],
-["8", "Release Time<br/>Fri 21/11/25, 4pm<hr/><b>Submission Deadline</b><br/>Thu 04/11/25<br/>01:00pm UK time<hr/><b>Submission Help</b><br/>Wed 26/11/25<br/>Wed 03/12/25<br/>10:00am-01:00pm<br/>MVB2.11/1.15",                     "",                                  "", "", "0", "0"],
+["8", "Release Time<br/>Fri 21/11/25, 4pm<hr/><b>Submission Deadline</b><br/>Thu 04/11/25<br/>01:00pm UK time<hr/>",                     "",                                  "", "", "0", "0"],
 //["7", "in your own time", "SKETCH ASSIGNMENT<br/>(30% of unit mark)<hr/>", "https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2025_TB-1/content/imperative/week10CW/index.html", "", "133", "19"],
 ["10", "",                     "",                                 "", "", "0", "0"],
 ["0", "Fri 22/11/24<br/>09:00pm-09:50pm<br/>CHEM BLDG LT1",   "ADVANCED CONCEPTS",  "https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2025_TB-1/content/imperative/COMS10016_24.pdf", "https://mediasite.bris.ac.uk/Mediasite/Play/ff1c3ff560064ad0b8101bbe6cb2c90d1d", "152", "7"],
 ["10", "",                     "",                                 "", "", "0", "0"],
-["8", "",                     "",                                  "", "", "0", "0"],
+["8", "<b>Submission Help</b><br/>Wed 26/11/25<br/>Wed 03/12/25<br/>10:00am-01:00pm<br/>MVB2.11/1.15",                     "",                                  "", "", "0", "0"],
  //["8", "<b>Submission Deadline</b><br/>Thu 28/11/24<br/>01:00pm UK time<hr/><b>Submission Help</b><br/>Weds 20/11/24<br/>Weds 27/11/24<br/>10:00am-01:00pm<br/>MVB2.11/1.15<hr/><a target='_blank' href='https://www.ole.bris.ac.uk/ultra/courses/_259156_1/outline'></a>", "", "", "0", "0"], 
 ////["8", "Support Labs<br/>Wed 30/11/22<br/>Wed 07/12/22<br/>10:00am-01:00pm<br/>MVB2.11/1.15<hr/>Extra Help via<br/><a href='https://teams.microsoft.com/l/channel/19%3a6bdf63f6ee8b4aab95c84f75f1b25e7e%40thread.tacv2/Week%252009-11%2520(Imp)%2520-%2520Sketch%2520Assignment?groupId=7830f761-4803-4b75-8a0d-36308e7d5c31&tenantId=b2e47f30-cd7d-4a4e-a5da-b18cf1a4151b' target='_blank'>MS Teams Channel</a><hr/>",                     "",                              "", "", "0", "0"],
  
@@ -517,6 +517,7 @@ var files = [
 [ "174", "https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2025_TB-1/content/imperative/self.c",                     "self.c"],
 [ "175", "https://www.ole.bris.ac.uk/bbcswebdav/courses/COMS10016_2025_TB-1/content/imperative/ring.c",                     "ring.c"],
 ];
+
 
 
 
